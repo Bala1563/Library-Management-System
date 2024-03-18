6ae4ab13bea34272daa30d3f51b75a81744b9cad
Bala <bala.venkatarama@zucisystems.com>
2024-03-18 12:01:08 +0530

    Add project files.

---
+++ Library/Repos/IBookRepo.cs
@@ -0,0 +1,28 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Repos
+{
+    public interface IBookRepo
+    {
+        //operations by user
+        Task<List<Book>> GetAllBooks();
+        Task<List<Book>> GetByAuthor(string author);
+        Task<List<Book>> GetByPublicationYear(DateTime publicationYear);
+        Task<List<Book>> GetByGenre(string genre);
+        Task<Book> GetBookById(int bookId);
+
+        Task BorrowBook(int bookId, int borrowerId);
+        Task ReturnBook(int bookId, int borrowerId);
+        Task<List<Book>> BorrowerBooks(int borrowerId);
+
+        //operations by admin only
+        Task InsertBook(Book book);
+        Task DeleteBook(int bookId);
+        Task UpdateBook(int bookId, Book book);
+    }
+}
